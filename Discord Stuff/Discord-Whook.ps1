bd274bda601ad71dbe7847c5ad2b044169d2bd7e
DanaMW <danamw@gmail.com>
2019-12-19 02:02:15 -0800

    update discord stuff

--- Discord Stuff/Discord-Whook.ps1
+++ Discord Stuff/Discord-Whook.ps1
@@ -1,32 +1,39 @@
-param([string]$MyArgs)
-$FileVersion = "Version: 0.0.3"
+﻿param([string]$MyArgs)
+$FileVersion = "Version: 0.0.8"
+$Space = "       "
 if ($myargs -eq "") {
-    $hookUrl = 'https://discordapp.com/api/webhooks/YourWebhookUrl'
+    $hookUrl = 'Webhook URL'
     $title0 = $Env:USERDOMAIN
     $title = $title0.substring(0, 1).toupper() + $title0.substring(1).tolower()
     $content = @"
-System Information From: $title
+Server Message $title
 No value was included as a parameter.
-Webhook: WzErO
+$Space
 "@
     $payload = [PSCustomObject]@{
         content = $content
     }
-    Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json)
+    Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'application/json'
     return
 }
 $TheArgs = "$myargs $args"
-$hookUrl = 'https://discordapp.com/api/webhooks/471880135226949633/YourWebhookUrl'
+$hookUrl = 'Webhook URL'
 $title0 = $Env:USERDOMAIN
 $title = $title0.substring(0, 1).toupper() + $title0.substring(1).tolower()
 
 $content = @"
-System Information From: $title
+Server Message From: $title
 $theArgs
+$Space
 "@
 
 $payload = [PSCustomObject]@{
     content = $content
 }
-Write-Host $payload
-Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json)
+#Say $payload
+Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'application/json'
+
+#$thumbnailObject = [PSCustomObject]@{
+#    url = "https://static1.squarespace.com/static/5644323de4b07810c0b6db7b/t/5aa44874e4966bde3633b69c/1520715914043/webhook_resized.png"
+#}
+#"thumbnail": "$thumbnailObject"
